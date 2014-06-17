e92d23292738be6cf22f641df3bc83381f7c35e7
Carmen Cojocaru <carmenn.cojocaru@gmail.com>
2014-06-18 00:23:12 +0300

    Adding the html for the history of reservations

---
+++ application/views/user/history.php
@@ -0,0 +1,38 @@
+<!-- fisier php ce afiseaza istoricul rezervarilor facute de utilizatorul curent -->
+<?php	
+	session_start(); 
+	// timpul minim de viata al sesiunii
+	$timeRange = 20;
+	// verificam daca exista lipsa de activitate
+	if(isset($_SESSION['timeout'])){
+		// calculeaza timpul de inactivitate
+		$sessionTime = time() - $_SESSION['timeout'];
+		echo "session time = " . $sessionTime;
+		echo "time range = " . $timeRange;
+		// si daca este mai mare decat timpul permis atunci
+		if($sessionTime > $timeRange){
+			// cand userul revine si incearca sa acceseze pagina, sesiunea se distruge
+			session_unset();
+			session_destroy();
+			// iar userul este delogat
+			header("Location: " . $logoutPage);
+		}
+	}
+	// stocheaza timpul in care a fost accesata ultima data pagina
+	$_SESSION['timeout'] = time();	
+?>
+<html>
+	<head>
+		<title><?php echo $title; ?></title>		
+		<link rel="stylesheet" type="text/css" href=<?php echo $css_file; ?> />
+		<script type="text/javascript" src=<?php echo $js_file; ?>></script>
+	</head>
+	<body>				
+		<section>
+			
+		</section>
+		<p id="footer">&copy; 2011 </p>
+	</body>
+</html>
+	
+
